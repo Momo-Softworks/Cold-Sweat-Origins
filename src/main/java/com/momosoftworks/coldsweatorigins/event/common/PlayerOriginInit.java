9b6fd9d85084062997cbc1e640f6446b25a7a26a
MikulDev <gamer3mc@gmail.com>
2024-06-05 22:32:00 -0400

    Fix attribute loading on player origin change & respawn

--- src/main/java/com/momosoftworks/coldsweatorigins/event/common/PlayerOriginInit.java
+++ src/main/java/com/momosoftworks/coldsweatorigins/event/common/PlayerOriginInit.java
@@ -1,18 +1,23 @@
 package com.momosoftworks.coldsweatorigins.event.common;
 
-import com.momosoftworks.coldsweat.api.util.Temperature;
-import com.momosoftworks.coldsweat.common.event.capability.EntityTempManager;
+import com.momosoftworks.coldsweat.common.capability.handler.EntityTempManager;
+import com.momosoftworks.coldsweat.core.event.TaskScheduler;
 import com.momosoftworks.coldsweatorigins.ColdSweatOrigins;
-import com.momosoftworks.coldsweatorigins.parsing.MappedAttribute;
 import com.momosoftworks.coldsweatorigins.parsing.AssignedAttributeModifier;
+import com.momosoftworks.coldsweatorigins.parsing.MappedAttribute;
 import com.momosoftworks.coldsweatorigins.parsing.OriginModifier;
 import io.github.edwinmindcraft.origins.api.OriginsAPI;
+import io.github.edwinmindcraft.origins.api.origin.Origin;
+import io.github.edwinmindcraft.origins.api.origin.OriginLayer;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.function.BiConsumer;
 
 @Mod.EventBusSubscriber
 public class PlayerOriginInit
@@ -24,30 +29,21 @@ public class PlayerOriginInit
         {
             EntityTempManager.getTemperatureCap(player).ifPresent(temp ->
             {
-                player.getCapability(OriginsAPI.ORIGIN_CONTAINER).ifPresent(origins ->
+                forEachOrigin(player, (layer, origin) ->
                 {
-                    origins.getOrigins().forEach((layer, origin) ->
+                    if (origin.location().toString().equals(player.getPersistentData().getString("CurrentOrigin")))
+                    {   return;
+                    }
+                    OriginModifier.ORIGIN_SETTINGS.computeIfPresent(origin.location(), (org, settings) ->
                     {
-                        OriginModifier.ORIGIN_SETTINGS.computeIfPresent(origin.location(), (org, settings) ->
+                        TaskScheduler.scheduleServer(() ->
                         {
                             ColdSweatOrigins.LOGGER.info("Applying origin settings for %s to %s", origin.location(), player.getName().getString());
-                            if (!player.getPersistentData().getString("Origin").equals(origin.location().toString()))
-                            {
-                                for (Temperature.Trait trait : EntityTempManager.VALID_ATTRIBUTE_TYPES)
-                                {   AttributeInstance attribute = EntityTempManager.getAttribute(trait, player);
-                                    temp.clearPersistentAttribute(attribute.getAttribute());
-                                    attribute.removeModifiers();
-                                    attribute.setBaseValue(attribute.getAttribute().getDefaultValue());
-                                }
-                            }
-                            else return settings;
-
                             for (MappedAttribute settingAttribute : settings.baseValues())
                             {
                                 AttributeInstance attribute = player.getAttribute(settingAttribute.attribute());
                                 if (attribute != null)
                                 {   attribute.setBaseValue(settingAttribute.value());
-                                    temp.markPersistentAttribute(settingAttribute.attribute());
                                 }
                             }
                             for (AssignedAttributeModifier modifier : settings.modifiers())
@@ -56,16 +52,19 @@ public class PlayerOriginInit
                                 if (attribute != null)
                                 {   attribute.addPermanentModifier(modifier);
                                 }
-                                temp.markPersistentAttribute(attribute.getAttribute());
                             }
+                        }, 5);
+                        player.getPersistentData().putString("CurrentOrigin", origin.location().toString());
 
-                            player.getPersistentData().putString("Origin", origin.location().toString());
-
-                            return settings;
-                        });
+                        return settings;
                     });
                 });
             });
         }
+    }
+
+    public static void forEachOrigin(Player player, BiConsumer<ResourceKey<OriginLayer>, ResourceKey<Origin>> consumer)
+    {
+        player.getCapability(OriginsAPI.ORIGIN_CONTAINER).ifPresent(origins -> origins.getOrigins().forEach(consumer));
     }
 }
