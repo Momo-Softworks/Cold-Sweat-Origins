1c830a4bfbf1216f514b876513ea6a9fb8381eea
MikulDev <gamer3mc@gmail.com>
2025-08-04 16:17:20 -0400

    Remove leftover project template code

--- src/main/java/com/momosoftworks/coldsweatorigins/ColdSweatOrigins.java
+++ src/main/java/com/momosoftworks/coldsweatorigins/ColdSweatOrigins.java
@@ -8,19 +8,13 @@ import com.momosoftworks.coldsweatorigins.parsing.OriginModifier;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(ColdSweatOrigins.MOD_ID)
 public class ColdSweatOrigins
 {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "cold_sweat_origins";
     public static final Logger LOGGER = LogManager.getFormatterLogger("Cold Sweat Origins");
 
@@ -35,9 +29,5 @@ public class ColdSweatOrigins
 
     public ColdSweatOrigins()
     {
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 }
